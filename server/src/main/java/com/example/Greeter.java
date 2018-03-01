fa56527afc2e2ef13c1e2ec3035b470dac8a140c
avi <dada220393@gmail.com>
2018-03-01 12:54:27 -0600

    5th fix checkstyle warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,17 +1,13 @@
 package com.example;
-
 /**
  * This is a class.
  */
 public class Greeter {
-
   /**
    * This is a constructor.
    */
   public Greeter() {
-
   }
- 
   /**
    *@param someone the name of a person
    *@return greeting string
